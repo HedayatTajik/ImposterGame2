aba56784da426d5586dd8c7c2b6564dd8e6efe65
Hedayatullah Tajik <HETAJIK@sma.de>
2025-11-08 13:54:22 +0100

    inject services

--- ImposterGame/Program.cs
+++ ImposterGame/Program.cs
@@ -1,4 +1,5 @@
 using ImposterGame;
+using ImposterGame.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Radzen;
@@ -6,7 +7,8 @@ using Radzen;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+builder.Services.AddScoped<WordService>(); 
+builder.Services.AddScoped<GameService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddRadzenComponents();
 await builder.Build().RunAsync();
